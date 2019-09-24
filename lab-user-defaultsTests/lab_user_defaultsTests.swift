1b65d6895a438d2a9476900cbcea32692d9136b7
levi davis <levidavis@pursuit.org>
2019-09-24 14:49:30 -0400

    First commit

---
+++ lab-user-defaultsTests/lab_user_defaultsTests.swift
@@ -0,0 +1,34 @@
+//
+//  lab_user_defaultsTests.swift
+//  lab-user-defaultsTests
+//
+//  Created by Levi Davis on 9/24/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import XCTest
+@testable import lab_user_defaults
+
+class lab_user_defaultsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
